c12a8aa59789b4bd714dee32ad72e5ba2eda1d0c
Santraj <santrajmaurya@gmail.com>
2021-05-19 12:07:33 +0530

    Placement-page-layout

---
+++ src/components/AddDetailsModal.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Modal, Form, Input, DatePicker } from "antd";
+import { observer } from "mobx-react";
+
+import "./style.scss";
+
+const AddDetailsModal = ({ closeModal, isModalVisible, details, title }) => {
+  const [form] = Form.useForm();
+
+  const handleOk = () => {
+    closeModal();
+  };
+
+  const handleCancel = () => {
+    closeModal();
+  };
+
+  console.log("details", details);
+
+  return (
+    <>
+      <Modal
+        title={title}
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText="Save"
+      >
+        {/* <Form form={form} name="addDeatils" labelCol={{ span: 8 }} wrapperCol={{ span: 14 }}>
+          {details.map((detail) => (
+            <Form.Item name={detail.name} label={detail.label} required>
+              {detail.type === "date" ? (
+                <DatePicker placeholder={detail.label} style={{width: '100%'}} />
+              ) : (
+                <Input placeholder={detail.label} type={detail.type} />
+              )}
+            </Form.Item>
+          ))}
+        </Form> */}
+        <Form form={form} name="addDeatils" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }}>
+          {details.map((detail) => (
+            <Form.Item name={detail.name} label={detail.label} required>
+              {detail.type === "date" ? (
+                <DatePicker placeholder={detail.label} style={{width: '100%'}} />
+              ) : detail.type === 'textarea' ? (
+                <Input.TextArea placeholder={detail.label} type={detail.type} />
+              ) : (
+                <Input placeholder={detail.label} type={detail.type} />
+              )}
+            </Form.Item>
+          ))}
+        </Form>
+      </Modal>
+    </>
+  );
+};
+
+export default observer(AddDetailsModal);
